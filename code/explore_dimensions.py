8ae3202e72ad5d28aec99cec04a85744a9d42d7d
Jason Yosinski <jason@yosinski.com>
2010-10-31 22:59:48 -0400

    Added explore_dimensions.py to collect data for individual dimension varying plots

---
+++ code/explore_dimensions.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+'''
+@date: 31 October 2010
+
+Explores each parameter dimension separately.
+
+Starts with a random location, or the one specified on the command line, if applicable
+'''
+
+import math, pdb, sys
+import random
+from datetime import datetime
+from copy import copy
+from Robot import Robot
+from SineModel import SineModel5
+from RunManager import RunManager
+from Neighbor import Neighbor
+
+
+
+def main():
+    runman = RunManager()
+
+    # Choose initialState, either from user-inputted parameters or randomly
+    if len(sys.argv) > 1:
+        initialState = [eval(xx) for xx in sys.argv[1].split()]
+    else:
+        initialState = runman.initialState(SineModel5.typicalRanges)
+
+    runman.explore_dimensions(initialState, SineModel5.typicalRanges, pointsPerDim = 9, repetitions = 2)
+
+
+
+if __name__ == '__main__':
+    main()
