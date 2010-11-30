365b2b0cf30f659dfc347e99b8f8545648db7dcb
Jason Yosinski <jason@yosinski.com>
2010-11-29 23:49:21 -0500

    About ready to run NEAT

--- code/ExternalStrategy.py
+++ code/ExternalStrategy.py
@@ -7,6 +7,7 @@
 #import pickle
 #import pdb
 
+from Robot import MIN_INNER, MAX_INNER, MIN_OUTER, MAX_OUTER
 from Strategy import Strategy, OneStepStrategy
 from util import matInterp
 from SineModel import SineModel5
@@ -21,16 +22,17 @@ class NEATStrategy(OneStepStrategy):
     def __init__(self, *args, **kwargs):
         super(NEATStrategy, self).__init__(*args, **kwargs)
 
-        self.executable = 'hyperNEAT'
-        self.motionFile = 'something.dat'
+        self.executable = '/home/team/s/h2_synced/HyperNEAT_v2_5/out/Hypercube_NEAT'
+        #self.motionFile = '/home/team/s/h2_synced/HyperNEAT_v2_5/out/spiderJointAngles.txt'
+        self.motionFile = 'spiderJointAngles.txt'
 
         self.proc = sp.Popen((self.executable
-                              '-a', '-b', 'c',),
+                              '-O', 'delme', '-R', '102', '-I', 'SpiderRobotExperiment.dat'),
                              stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
 
     def __del__(self):
         print 'Waiting for %s to exit...' % self.executable
-        code = proc.wait()
+        code = self.proc.wait()
         print 'Done.'
 
 
@@ -46,8 +48,23 @@ class NEATStrategy(OneStepStrategy):
                 positions = vstack((positions, array(nums)))
             except NameError:
                 positions = array(nums)
-        times = linspace(0,12,positions.shape[0])
 
+        # swap and scale positions appropriately
+        positions = positions.T[ix_([0,1,4,5,2,3,6,7])].T  # remap to right columns
+        # scale from [-1,1] to [0,1]
+        positions += 1
+        positions *= .5
+        # scale from [0,1] to appropriate ranges
+        innerIdx = [0, 2, 4, 6]
+        outerIdx = [1, 3, 5, 7]
+        for ii in innerIdx:
+            positions[:,ii] *= (MAX_INNER - MIN_INNER)
+            positions[:,ii] += MIN_INNER
+        for ii in outerIdx:
+            positions[:,ii] *= (MAX_OUTER - MIN_OUTER)
+            positions[:,ii] += MIN_OUTER
+        times = linspace(0,12,positions.shape[0])
+        
         # return function of time
         return lambda time: matInterp(time, times, positions)
 
@@ -64,12 +81,20 @@ class NEATStrategy(OneStepStrategy):
         super(NEATStrategy, self).updateResults(dist)
 
         # Send fitness to process
-        out,err = proc.communicate('%f\n' % dist)
+        #out,err = proc.communicate('%f\n' % dist)
+
+        print 'Sending to process: %f' % dist)
+        self.proc.stdin.write('%f\n' % dist)
+
+        print 'TRYING READ STDOUT'
+        stdout = self.proc.stdout.read()
+        print 'TRYING READ STDOUT'
+        stderr = self.proc.stderr.read()
 
         print 'Got stdout:'
-        print out
+        print stdout
         print 'Got stderr:'
-        print err
+        print stderr
 
 
     def logHeader(self):
