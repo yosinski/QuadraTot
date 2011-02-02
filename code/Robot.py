087a511f01900855ed4166118de5b639308ad1e2
Jason Yosinski <jason@yosinski.com>
2011-02-02 14:30:42 -0500

    Added script to convert from _filt files to _cmd files and two generated cmd files

--- code/Robot.py
+++ code/Robot.py
@@ -21,27 +21,7 @@ individually.  More complex collisions are still possible given
 certain vectors of motor position.
 '''
 
-MIN_INNER = 150
-MAX_INNER = 770
-MIN_OUTER = 30
-#MAX_OUTER = 940
-#MAX_OUTER = 800 # changing because Robot hits antenna
-MAX_OUTER = 680  # changing because screws still interfere
-#MIN_CENTER = 512 - 180
-#MAX_CENTER = 512 + 180
-MIN_CENTER = 512 - 120
-MAX_CENTER = 512 + 120
-NORM_CENTER = 512
-
-POS_FLAT      = [512] * 9
-POS_READY     = [770,  40] * 4 + [512]
-POS_HALFSTAND = [700, 100] * 4 + [512]
-POS_STAND     = [512, 150] * 4 + [512]
-
-POS_CHECK_1   = [770, 200] * 4 + [512]
-POS_CHECK_2   = [670, 300] * 4 + [512]
-POS_CHECK_3   = [670, 300] * 4 + [512-70]
-
+from RobotConstants import *
 
 
 class RobotFailure(Exception):
@@ -236,7 +216,7 @@ class Robot():
 
         
     def interpMove(self, start, end, seconds):
-        '''Moves between start and end over seconds seconds.  stard
+        '''Moves between start and end over seconds seconds.  start
         and end may be functions of the time.'''
 
         self.updateClock()
