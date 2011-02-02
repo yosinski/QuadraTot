674238f454059fc609feb8f0dc4f18c3d2a8d46e
Jason Yosinski <jason@yosinski.com>
2011-02-02 15:27:35 -0500

    Added printing of constants

--- code/RobotConstants.py
+++ code/RobotConstants.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 MIN_INNER = 150
 MAX_INNER = 770
 MIN_OUTER = 30
@@ -18,3 +20,16 @@ POS_STAND     = [512, 150] * 4 + [512]
 POS_CHECK_1   = [770, 200] * 4 + [512]
 POS_CHECK_2   = [670, 300] * 4 + [512]
 POS_CHECK_3   = [670, 300] * 4 + [512-70]
+
+
+
+
+if __name__ == '__main__':
+    toprint = ('MIN_INNER', 'MAX_INNER',
+               'MIN_OUTER', 'MAX_OUTER',
+               'MIN_CENTER', 'MAX_CENTER',
+               'POS_FLAT', 'POS_READY', 'POS_HALFSTAND', 'POS_STAND')
+
+    #pdb.set_trace()
+    for var in toprint:
+        print '%14s = %s' % (var, repr(eval(var)))
